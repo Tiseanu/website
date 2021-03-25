cd666e621bff515f8695f5ac39b37b0ce4a6f4e3
LTiseanu <ltiseanu@yahoo.com>
2021-03-25 09:04:05 +0200

    Updates

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -8,30 +8,33 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 export class BannerComponent implements OnInit {
   motto = "Every day is a second chance!";
 
+  aboutMe = `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nisl turpis, porttitor et finibus id, viverra a metus. Praesent non ante sed orci posuere varius quis sit amet dui. Cras molestie magna orci, id gravida dolor molestie in. Duis sollicitudin turpis quis tortor egestas, ut ultrices nisl elementum. Vestibulum sed ipsum eget nulla laoreet cursus in ac sem. Integer a suscipit justo, quis aliquam sapien. Maecenas et tellus nibh. Vivamus tincidunt eros id commodo pellentesque.</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nisl turpis, porttitor et finibus id, viverra a metus. Praesent non ante sed orci posuere varius quis sit amet dui. Cras molestie magna orci, id gravida dolor molestie in. Duis sollicitudin turpis quis tortor egestas, ut ultrices nisl elementum. Vestibulum sed ipsum eget nulla laoreet cursus in ac sem. Integer a suscipit justo, quis aliquam sapien. Maecenas et tellus nibh. Vivamus tincidunt eros id commodo pellentesque.</p>`;
+
   work = [
     {
       image: "https://picsum.photos/200",
       title: "Project",
-      text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-      time: "9m"
+      text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is.",
+      time: "9Weeks"
     },
     {
       image: "https://picsum.photos/201",
       title: "Project",
-      text: "His is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-      time: "6m"
+      text: "His is a wider card with supporting text below as a natural lead-in to additional content. This content is a little.",
+      time: "2Weeks"
     },
     {
       image: "https://picsum.photos/202",
       title: "Project",
       text: "Is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-      time: "10m"
+      time: "2Months"
     },
     {
       image: "https://picsum.photos/204",
       title: "Project",
       text: "A wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-      time: "7m"
+      time: "10Days"
     },
   ];
 
